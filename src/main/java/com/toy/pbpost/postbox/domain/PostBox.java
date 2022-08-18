64c4bcaca8f3a4a2a288b8db5176a3496281da48
JongChan <kjc3590@songareeit.com>
2022-08-18 16:50:11 +0900

    [Letter] return관련 작업 추가 및 랜드마크 조회 추가

--- src/main/java/com/toy/pbpost/postbox/domain/PostBox.java
+++ src/main/java/com/toy/pbpost/postbox/domain/PostBox.java
@@ -2,13 +2,17 @@ package com.toy.pbpost.postbox.domain;
 
 import com.toy.pbpost.common.domain.Address;
 import com.toy.pbpost.common.domain.BaseTimeEntity;
+import com.toy.pbpost.letter.domain.Letter;
 import com.toy.pbpost.user.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
@@ -30,6 +34,11 @@ public class PostBox extends BaseTimeEntity {
 
     @Embedded
     private Address address;
+
+    @Builder.Default
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "toPostBox")
+    @Where(clause = "return_time >= CURRENT_TIMESTAMP")
+    private List<Letter> letterList = new ArrayList<>();
 
     public void setAddress(Address address) {
         this.address = address;
